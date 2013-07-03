59dd9a3ce822d3dfa0a5051bdf1fa17a4792f7d7
Jared De Blander <jared0x90@gmail.com>
2013-07-03 01:21:27 +0000

    Static files, templating

--- ziptrades.py
+++ ziptrades.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env pypy
 from flask import Flask
-app = Flask(__name__)
+from flask import render_template
+
+app = Flask(__name__, static_folder='static', static_url_path='/static')
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template('index.html')
+
+@app.route("/post")
+def post_listing():
+    return render_template('post.html')
+
+@app.route("/layout")
+def layout():
+    return render_template('layout.html')
 
 if __name__ == "__main__":
     app.run(host = '0.0.0.0', port = 80)
